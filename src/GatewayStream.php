38711f447b50c52a53ee7d5613b3b7fab6c3141d
abhayalphansotech <abhay@alphansotech.com>
2024-03-06 10:15:35 +0530

    Testing Global Variable

--- src/GatewayStream.php
+++ src/GatewayStream.php
@@ -2,8 +2,13 @@
 namespace Alphansotech\GatewayStreaming;
 
 class GatewayStream{
+	function __construct(){
+		$wsc_api_key = 'wsc_api_key_detail';
+		$wsc_access_key = 'wsc_access_key_detail';
+	}
+
 	function sayHi(){
-		return "Hi ".rand(0,1000);
+		return "Hi ".rand(0,1000).' I am '.$this->wsc_api_key;
 	}
 
 	function sayHi1(){

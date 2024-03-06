99287a074e2d4e75a475ad22681bd01bb37aef09
abhayalphansotech <abhay@alphansotech.com>
2024-03-06 10:34:57 +0530

    Testing Global Variable

--- src/GatewayStream.php
+++ src/GatewayStream.php
@@ -3,16 +3,38 @@ namespace Alphansotech\GatewayStreaming;
 
 class GatewayStream{
 	public $wsc_api_key = '';
-	function __construct(){
-		$this->wsc_api_key = 'wsc_api_key_detail';
-		$wsc_access_key = 'wsc_access_key_detail';
+	public $wsc_access_key = '';
+	public $wsc_api_baseurl = '';
+	function __construct($wsc_api_key_id, $wsc_access_key_id){
+		$this->wsc_api_key = $wsc_api_key_id;
+		$this->wsc_access_key = $wsc_access_key_id;
+		$this->wsc_api_baseurl = 'https://api.video.wowza.com/api/v1.8';
 	}
 
 	function sayHi(){
 		return "Hi ".rand(0,1000).' I am '.$this->wsc_api_key;
 	}
 
-	function sayHi1(){
-		return "Hi12 ".rand(0,1000);
+	public function GetLiveStreaming($streamingId) {
+		$url = $this->wsc_api_baseurl."/live_streams/$streamingId";
+		$header = [
+			"Content-Type:"  	. "application/json",
+			"charset:"			. "utf-8",
+			"wsc-api-key:"		. $this->wsc_api_key,
+			"wsc-access-key:"	. $this->wsc_access_key,
+		];
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST , "GET");
+		curl_setopt($ch, CURLOPT_URL,$url);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
+		$server_output = curl_exec($ch);
+		$err = curl_error($ch);
+		curl_close ($ch);
+		$output = json_decode($server_output);
+		return $output;
 	}
 }
